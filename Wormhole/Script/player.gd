68c1abc85e30fa54d74d77ac3174a678553dce8c
NitenOS <benosuke@protonmail.com>
2024-01-18 02:26:47 +0100

    Level test
    - Setting up of a simple test level.
    - Add cross who rotate. Now we see the direction of our ship/player
    - Many (many) personal playtest

--- Wormhole/Script/player.gd
+++ Wormhole/Script/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody3D
 
 const SPEED : float = 0.05
 const SENS : float = 3
+const MAX_SPEED : int = 10
 
 var can_shoot : bool = true
 var dead : bool = false
@@ -20,7 +21,6 @@ func _input(event):
 	if dead:
 		return
 	if event is InputEventMouseMotion:
-		#rotation_degrees.y -= event.relative.x * SENS
 		pass
 	
 	pass
@@ -38,6 +38,8 @@ func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 		pass
+	arrow_direction()
+		
 	
 	pass
 
@@ -45,14 +47,21 @@ func _physics_process(delta):
 	if dead:
 		return
 	var input_dir = Input.get_axis("move_forwards", "move_backwards")
+	var input_stafe = Input.get_axis("strafe_left", "strafe_right") 
 	var direction = (transform.basis * Vector3(0, 0, input_dir)).normalized()
+	
 	if Input.is_action_just_pressed("strafe_left") or Input.is_action_just_pressed("strafe_right") :
-		var input_stafe = Input.get_axis("strafe_left", "strafe_right") 
 		direction += (transform.basis * Vector3(input_stafe, 0, 0)).normalized() * 50
-	if direction:
+		
+	if direction and velocity.length() < MAX_SPEED:
 		velocity.x += direction.x * SPEED
 		velocity.z += direction.z * SPEED
-	print(velocity)
+	
+	if velocity.length() > MAX_SPEED:
+		velocity.x = move_toward(velocity.x, 0, 0.1)
+		velocity.z = move_toward(velocity.z, 0, 0.1)
+		
+	print(velocity.x)
 	
 	var input_orientation = Input.get_axis("watch_left", "watch_right")
 	rotation_degrees.y -= input_orientation * SENS
@@ -77,6 +86,14 @@ func shoot():
 
 func shoot_anim_done():
 	can_shoot = true
+	pass
+	
+func arrow_direction():
+	$CanvasLayer/Arrow.rotation_degrees = rotation_degrees.y
+	$CanvasLayer/Arrow/right_arrow.modulate.a = velocity.x * 100 / 10 /100
+	$CanvasLayer/Arrow/left_arrow.modulate.a = velocity.x * 100 / -10 /100
+	$CanvasLayer/Arrow/down_arrow.modulate.a = velocity.z * 100 / 10 /100
+	$CanvasLayer/Arrow/up_arrow.modulate.a = velocity.z * 100 / -10 /100
 	pass
 
 func kill():
